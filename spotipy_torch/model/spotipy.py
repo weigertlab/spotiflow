5d85bff45d86ef85c30d37f5ea5beb9933c15a99
AlbertDominguez <albert.dominguezmantes@epfl.ch>
2023-09-01 17:20:34 +0200

    Fix predictions

--- spotipy_torch/model/spotipy.py
+++ spotipy_torch/model/spotipy.py
@@ -277,7 +277,7 @@ class Spotipy(nn.Module):
                 )  # Add B and C dimensions
                 img_t = img_t.permute(0, 3, 1, 2)
                 y = (
-                    self._sigmoid(self(img_t)[0].squeeze(0).squeeze(0))
+                    self._sigmoid(self(img_t)["heatmaps"][0].squeeze(0).squeeze(0))
                     .detach()
                     .cpu()
                     .numpy()
