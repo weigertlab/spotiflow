42705224d022a2d5681cc3d062019d5dd99bb68e
AlbertDominguez <albert.dominguezmantes@epfl.ch>
2023-09-01 13:23:34 +0200

    Fix threshold optimization. Fix wandb logging the same image at every step.

--- spotipy_torch/model/trainer.py
+++ spotipy_torch/model/trainer.py
@@ -189,24 +189,24 @@ class SpotipyTrainingWrapper(pl.LightningModule):
                 images=[
                     v.transpose(1, 2, 0) for v in self._valid_inputs[:n_images_to_log]
                 ],
-                step=self.current_epoch,
+                step=self.global_step,
             )
             self.logger.log_image(
                 key="target",
                 images=[cm.magma(v) for v in self._valid_targets[:n_images_to_log]],
-                step=self.current_epoch,
+                step=self.global_step,
             )
             self.logger.log_image(
                 key="output",
                 images=[cm.magma(v) for v in self._valid_outputs[:n_images_to_log]],
-                step=self.current_epoch,
+                step=self.global_step,
             )
             self.logger.log_image(
                 key="flow",
                 images=[
                     0.5 * (1 + v.transpose(1,2,0)) for v in self._valid_flows[:n_images_to_log]
                 ],
-                step=self.current_epoch,
+                step=self.global_step,
             )
 
         elif isinstance(
