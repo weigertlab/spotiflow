7fc8738739d1c46963b8ee5015b56f33ad1f5418
Martin Weigert <maweigert@users.noreply.github.com>
2023-11-09 16:59:43 +0100

    merged

--- spotipy_torch/data/__init__.py
+++ spotipy_torch/data/__init__.py
@@ -1,2 +1,9 @@
 # ruff: noqa: F401
 from .spots import SpotsDataset, collate_spots
+
+
+def test_data_hybiss_2d():
+    from tifffile import imread
+    from pathlib import Path
+
+    return imread(Path(__file__).parent / "_example_data" / "hybiss_2d.tif")
