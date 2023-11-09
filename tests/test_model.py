1f890e2ab2f21725a7fdf0300bf060542aeefbf3
Martin Weigert <maweigert@users.noreply.github.com>
2023-11-10 00:13:04 +0100

    adjust test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,8 @@
 from spotipy_torch.model import Spotipy
-
+from spotipy_torch.utils import normalize
+from spotipy_torch.data import test_data_hybiss_2d
 
 if __name__ == "__main__":
     model = Spotipy.from_pretrained("hybiss", map_location="cpu")
+    x = test_data_hybiss_2d()
+    points, _ = model.predict(normalize(x), device="cpu")
